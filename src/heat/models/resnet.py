133f670417f13aaeb7d985d226e39798e8bd2f57
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2025-07-15 12:01:26 +0900

    blackで整形

--- src/heat/models/resnet.py
+++ src/heat/models/resnet.py
@@ -19,7 +19,9 @@ class ResNetBackbone(nn.Module):
         self.conv_original_size0 = convrelu(3, 64, 3, 1)
         self.conv_original_size1 = convrelu(64, 64, 3, 1)
         self.layer0 = nn.Sequential(*self.base_layers[:3])  # size=(N, 64, x.H/2, x.W/2)
-        self.layer1 = nn.Sequential(*self.base_layers[3:5])  # size=(N, 64, x.H/4, x.W/4)
+        self.layer1 = nn.Sequential(
+            *self.base_layers[3:5]
+        )  # size=(N, 64, x.H/4, x.W/4)
         self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
         self.layer3 = self.base_layers[6]  # size=(N, 256, x.H/16, x.W/16)
         self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
@@ -37,8 +39,14 @@ class ResNetBackbone(nn.Module):
         layer4 = self.layer4(layer3)
 
         xs = {"0": layer2, "1": layer3, "2": layer4}
-        all_feats = {'layer0': layer0, 'layer1': layer1, 'layer2': layer2,
-                     'layer3': layer3, 'layer4': layer4, 'x_original': x_original}
+        all_feats = {
+            "layer0": layer0,
+            "layer1": layer1,
+            "layer2": layer2,
+            "layer3": layer3,
+            "layer4": layer4,
+            "x_original": x_original,
+        }
 
         mask = torch.zeros(inputs.shape)[:, 0, :, :].to(layer4.device)
         return xs, mask, all_feats
@@ -50,7 +58,7 @@ class ResNetBackbone(nn.Module):
             # fix all bn layers
             def set_bn_eval(m):
                 classname = m.__class__.__name__
-                if classname.find('BatchNorm') != -1:
+                if classname.find("BatchNorm") != -1:
                     m.eval()
 
             self.apply(set_bn_eval)
@@ -68,7 +76,9 @@ class ResNetUNet(nn.Module):
 
         self.layer0 = nn.Sequential(*self.base_layers[:3])  # size=(N, 64, x.H/2, x.W/2)
         # self.layer0_1x1 = convrelu(64, 64, 1, 0)
-        self.layer1 = nn.Sequential(*self.base_layers[3:5])  # size=(N, 64, x.H/4, x.W/4)
+        self.layer1 = nn.Sequential(
+            *self.base_layers[3:5]
+        )  # size=(N, 64, x.H/4, x.W/4)
         # self.layer1_1x1 = convrelu(256, 256, 1, 0)
         self.layer2 = self.base_layers[5]  # size=(N, 128, x.H/8, x.W/8)
         # self.layer2_1x1 = convrelu(512, 512, 1, 0)
@@ -77,7 +87,7 @@ class ResNetUNet(nn.Module):
         self.layer4 = self.base_layers[7]  # size=(N, 512, x.H/32, x.W/32)
         # self.layer4_1x1 = convrelu(2048, 2048, 1, 0)
 
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
 
         self.conv_up3 = convrelu(1024 + 2048, 1024, 3, 1)
         self.conv_up2 = convrelu(512 + 1024, 512, 3, 1)
@@ -161,7 +171,7 @@ class ResNetUNet(nn.Module):
             # fix all bn layers
             def set_bn_eval(m):
                 classname = m.__class__.__name__
-                if classname.find('BatchNorm') != -1:
+                if classname.find("BatchNorm") != -1:
                     m.eval()
 
             self.apply(set_bn_eval)
